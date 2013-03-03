aeb5320169e77b86e54155350716c1b9e6e82abb
Andrei Avram <andrei.avram@qualitance.ro>
2013-03-03 07:31:16 +0200

    setting fixes

--- scoutfile3/settings.py
+++ scoutfile3/settings.py
@@ -90,7 +90,7 @@ MEDIA_ROOT = '%s/media/' % FILE_ROOT
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '%smedia/scoutfile/' % URL_ROOT
+MEDIA_URL = '%smedia/' % URL_ROOT
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -100,7 +100,7 @@ STATIC_ROOT = '%s/static/' % FILE_ROOT
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '%sstatic/scoutfile/' % URL_ROOT 
+STATIC_URL = '%sstatic/' % URL_ROOT 
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
