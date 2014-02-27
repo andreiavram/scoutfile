34cc0a0e93a296d4d2f925a7e83a88cad86f3a11
Andrei Avram <andrei.avram@gmail.com>
2014-02-27 14:40:36 +0200

    refs #77 adaugat migrare catre InformatieContact (adresa -> adresa_postala)

---
+++ scoutfile3/structuri/management/commands/structuri.py
@@ -0,0 +1,35 @@
+from django.contrib.contenttypes.models import ContentType
+from structuri.models import Membru, InformatieContact, TipInformatieContact
+
+__author__ = 'andrei'
+
+import logging
+import datetime
+
+logger = logging.getLogger(__name__)
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    available_commands = ["update_adrese"]
+
+    def handle(self, *args, **options):
+        if args[0] not in self.available_commands:
+            self.stderr.write(u"Command must be one of %s" % ",".join(self.available_commands))
+
+        getattr(self, args[0])(*args[1:], **options)
+
+    def update_adrese(self, *args, **options):
+        total = 0
+        for membru in Membru.objects.all():
+            adrese = InformatieContact.objects.filter(content_type=ContentType.objects.get(membru), object_id=membru.id,
+                                                 tip_informatie__nume__iexact=u"adresă corespondență")
+            if adrese.count():
+                continue
+
+            ic = InformatieContact(content_type=ContentType.objects.get(membru), object_id=membru.id,
+                                   tip_infomatie=TipInformatieContact.objects.get(nume__iexact=u"adresă coresponență"),
+                                   valoare=membru.adresa, data_start=datetime.datetime.now())
+            ic.save()
+            total += 1
--- scoutfile3/structuri/models.py
+++ scoutfile3/structuri/models.py
@@ -287,6 +287,14 @@ class Membru(Utilizator):
         return super(Membru, self).save(*args, **kwargs)
 
     @property
+    def adresa_postala(self):
+        try:
+            return InformatieContact.objects.filter(content_type=ContentType.objects.get(self), object_id=self.id,
+                                                 tip_informatie__nume__iexact=u"adresă corespondență").valoare
+        except Exception, e:
+            return self.adresa
+
+    @property
     def centru_local(self):
         return self.get_centru_local()
 
@@ -750,4 +758,3 @@ class InformatieContact(models.Model):
 
     def __unicode__(self):
         return "%s: %s" % (self.tip_informatie.nume, self.valoare)
-    
