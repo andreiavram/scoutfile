63c336fbcd8e6a19066f8d5a639f6a40d361c1a7
Andrei Avram <andrei.avram@gmail.com>
2014-02-28 03:28:03 +0200

    refs #77 adaugat migrare catre InformatieContact (adresa -> adresa_postala)

--- scoutfile3/structuri/management/commands/structuri.py
+++ scoutfile3/structuri/management/commands/structuri.py
@@ -25,12 +25,12 @@ class Command(BaseCommand):
         total = 0
         for membru in Membru.objects.all():
             adrese = InformatieContact.objects.filter(content_type=ContentType.objects.get_for_model(membru), object_id=membru.id,
-                                                 tip_informatie__nume__iexact=u"adresă corespondență")
+                                                 tip_informatie__nume__iexact=u"Adresă corespondență", tip_informatie__relevanta="Membru")
             if adrese.count():
                 continue
 
             ic = InformatieContact(content_type=ContentType.objects.get_for_model(membru), object_id=membru.id,
-                                   tip_infomatie=TipInformatieContact.objects.get(nume__iexact=u"adresă coresponență"),
+                                   tip_infomatie=TipInformatieContact.objects.get(nume__iexact=u"Adresă corespondență", relevanta="Membru"),
                                    valoare=membru.adresa, data_start=datetime.datetime.now())
             ic.save()
             total += 1
