62ab477a186d1bc5faa2cb07a610f5a69c1158b3
Andrei Avram <andrei.avram@gmail.com>
2014-04-28 18:08:21 +0300

    rezolva unele probleme cu cotizatia sociala

--- scoutfile3/documente/admin.py
+++ scoutfile3/documente/admin.py
@@ -1,6 +1,6 @@
 # coding: utf8
 from documente.models import PlataCotizatieTrimestru, ChitantaCotizatie, Registru, Adeziune, DocumentCotizatieSociala, \
-    Document
+    Document, AsociereDocument
 from django.contrib import admin
 from django.contrib.admin.options import ModelAdmin
 
@@ -10,3 +10,4 @@ admin.site.register(Registru)
 admin.site.register(Adeziune)
 admin.site.register(Document)
 admin.site.register(DocumentCotizatieSociala)
+admin.site.register(AsociereDocument)
