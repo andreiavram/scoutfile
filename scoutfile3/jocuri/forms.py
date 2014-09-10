9717e063f70f71d76db6cc344851b1ffa9f2b5d1
Andrei Avram <andrei.avram@spyhce.com>
2014-09-10 04:04:38 +0300

    worked on jocuri: added markdown, updated detail page

--- scoutfile3/jocuri/forms.py
+++ scoutfile3/jocuri/forms.py
@@ -1,7 +1,8 @@
 # coding: utf-8
-from boto.beanstalk.exception import ValidationError
+from django.core.exceptions import ValidationError
 from goodies.forms import CrispyBaseModelForm
 from jocuri.models import FisaActivitate
+from django import forms
 
 __author__ = 'andrei'
 
@@ -10,9 +11,45 @@ class FisaActivitateForm(CrispyBaseModelForm):
     class Meta:
         model = FisaActivitate
         fields = ("titlu", "descriere_joc", "materiale_necesare", "ramuri_de_varsta",
-                    "min_participanti", "max_participanti", "min_durata", "max_durata",
+                    "min_participanti", "max_participanti",
                     "obiective_educative", "categorie", "sursa")
+
+    min_durata_string = forms.CharField(required=False, label=u"Durată minimă", help_text=u"Folosește expresii de tipul 2h15m sau 1z3h30m sau 2h sau 12m")
+    max_durata_string = forms.CharField(required=False, label=u"Durată maximă", help_text=u"Folosește expresii de tipul 2h15m sau 1z3h30m sau 2h sau 12m")
 
     def clean(self):
         if not "max_participanti" in self.cleaned_data and not "min_participanti" in self.cleaned_data:
             raise ValidationError(u"Măcar un număr minim sau un număr maxim de participanți trebuie completat")
+        elif self.cleaned_data.get("min_participanti") >= self.cleaned_data.get("max_participanti"):
+            raise ValidationError(u"Minimul de participanți nu poate fi mai mare decât maximul")
+
+        return self.cleaned_data
+
+    def clean_min_durata_string(self):
+        return self._clean_time_string(self.cleaned_data.get("min_durata", ""))
+
+    def clean_max_durata_string (self):
+        return self._clean_time_string(self.cleaned_data.get("max_durata", ""))
+
+    def _clean_time_string(self, value):
+        string_order = {
+            "s": 60 * 60 * 2400 * 7,
+            "z": 60 * 60 * 2400,
+            "h": 60 * 60,
+            "m": 60,
+        }
+
+        regex = r"(\d+s){0,1} *(\d+z){0,1} *(\d+h){0,1} *(\d+m){0,1}"
+        import re
+        results = re.findall(regex, value, re.IGNORECASE)
+        if len(results) == 0:
+            raise ValidationError("String invalid pentru timp. Foloseste s pentru saptamani, z pentru zile, h pentru ore, m pentru minute. 5 ore jumatate ar fi: 5h30m")
+
+        seconds = 0
+        for result in results[0]:
+            result = result.strip()
+            if not result:
+                continue
+            seconds += int(result[0:-1]) * string_order.get(results[-1])
+
+        return seconds
