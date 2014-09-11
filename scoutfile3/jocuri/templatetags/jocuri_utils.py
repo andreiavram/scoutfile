7d6943039402312b052dba924d60cb93aa1e2767
Andrei Avram <andrei.avram@gmail.com>
2014-09-12 02:29:53 +0300

    added some extra form validation, some form and flow fixes

--- scoutfile3/jocuri/templatetags/jocuri_utils.py
+++ scoutfile3/jocuri/templatetags/jocuri_utils.py
@@ -3,6 +3,7 @@ __author__ = 'andrei'
 
 from django import template
 from django.template.defaultfilters import stringfilter
+import datetime
 
 register = template.Library()
 
@@ -16,10 +17,30 @@ def numar_participanti_string(value):
         return u"Maxim %d" % value.max_participanti
     return u" Între %s și %s" % (value.min_participanti, value.max_participanti)
 
+
+def seconds_to_string(value):
+    td = datetime.timedelta(seconds=value)
+    weeks, days, hours, minutes = td.days // 7, td.days % 7, td.seconds // 3600, td.seconds // 60 % 60
+    output = ""
+    if weeks:
+        output += u"%d săptămâni"
+    if days:
+        output += ", " if len(output) else ""
+        output += "%d zile" % days
+    if hours:
+        output += ", " if len(output) else ""
+        output += "%d ore" % hours
+    if minutes:
+        output += ", " if len(output) else ""
+        output += "%d minute" % minutes
+    return output
+
 @register.filter
 def durata_string(value):
-    if value.min_durata is not None and value.max_durata is None:
-        return u"Cel puțin %d" % value.min_durata
-    if value.min_durata is None and value.max_durata is not None:
-        return u"Cel mult %d" % value.max_durata
-    return u"Între %s și %s" % (value.min_durata, value.max_durata)
+    if not value.min_durata and not value.max_durata:
+        return u"-"
+    if value.min_durata and not value.max_durata:
+        return u"Cel puțin %s" % seconds_to_string(value.min_durata)
+    if not value.min_durata and value.max_durata:
+        return u"Cel mult %s" % seconds_to_string(value.max_durata)
+    return u"Între %s și %s" % (seconds_to_string(value.min_durata), seconds_to_string(value.max_durata))
