b9593d510cd5f0e5fae3ae70e97cf14267b7f5fb
Andrei Avram <andrei.avram@gmail.com>
2014-09-21 03:12:37 +0300

    aranjat formular editare jocuri

--- scoutfile3/jocuri/forms.py
+++ scoutfile3/jocuri/forms.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from crispy_forms.layout import Layout, Field, MultiField, Div
 from django.core.exceptions import ValidationError
 from django.forms.widgets import TextInput, Textarea
 from django_markdown.widgets import MarkdownWidget
@@ -12,46 +13,6 @@ from django import forms
 from structuri.models import RamuraDeVarsta
 
 __author__ = 'andrei'
-
-
-class FisaActivitateForm(CrispyBaseModelForm):
-    class Meta:
-        model = FisaActivitate
-        fields = ("titlu", "descriere", "descriere_joc", "materiale_necesare", "ramuri_de_varsta",
-                    "min_participanti", "max_participanti",
-                    "obiective_educative", "categorie", "sursa", "tags")
-
-    min_durata_string = forms.CharField(required=False, label=u"Durată minimă", help_text=u"Folosește expresii de tipul 2h15m sau 1z3h30m sau 2h sau 12m")
-    max_durata_string = forms.CharField(required=False, label=u"Durată maximă", help_text=u"Folosește expresii de tipul 2h15m sau 1z3h30m sau 2h sau 12m")
-    descriere_joc = forms.CharField(required=True, label=u"Descriere", help_text=u"Format Markdown, descrierea jocului, ce trebuie pregătit, ce trebuie făcut, care este obiectivul, care sunt regulile ...", widget=MarkdownWidget)
-    descriere = forms.CharField(required=False, label=u"Descriere pe scurt", help_text=u"Un text de descriere pentru căutare", widget=Textarea())
-    ramuri_de_varsta = forms.ModelMultipleChoiceField(RamuraDeVarsta.objects.all(), required=True, label=u"Ramuri de vârstă", help_text=u"Alegeți mărcar una", widget=forms.CheckboxSelectMultiple)
-    tags = TagField(required=False, widget=TaggitTagsInput, label=u"Tag-uri")
-
-
-    def clean(self):
-        if not "max_participanti" in self.cleaned_data and not "min_participanti" in self.cleaned_data:
-            raise ValidationError(u"Măcar un număr minim sau un număr maxim de participanți trebuie completat")
-        elif self.cleaned_data.get("min_participanti", 0) >= self.cleaned_data.get("max_participanti", 10000):
-            raise ValidationError(u"Minimul de participanți nu poate fi mai mare decât maximul")
-
-        if "max_durata_string" in self.cleaned_data and "min_durata_string" in self.cleaned_data:
-            if self.cleaned_data.get("min_durata_string") >= self.cleaned_data.get("max_durata_string"):
-                self.errors["min_durata_string"] = ["Durata minimă trebuie să fie mai mică decât cea maximă", ]
-                raise ValidationError(u"Durata minimă trebuie să fie mai mică decât cea maximă")
-
-        return self.cleaned_data
-
-    def clean_min_durata_string(self):
-        return self._clean_time_string(self.cleaned_data.get("min_durata_string", ""))
-
-    def clean_max_durata_string (self):
-        return self._clean_time_string(self.cleaned_data.get("max_durata_string", ""))
-
-    @staticmethod
-    def _clean_time_string(value):
-        return parse_string_to_seconds(value)
-
 
 def parse_string_to_seconds(value, silent=False):
     string_order = {
@@ -78,6 +39,58 @@ def parse_string_to_seconds(value, silent=False):
 
     return seconds
 
+class FisaActivitateForm(CrispyBaseModelForm):
+    class Meta:
+        model = FisaActivitate
+        fields = ("titlu", "descriere", "descriere_joc", "materiale_necesare", "ramuri_de_varsta",
+                    "min_participanti", "max_participanti",
+                    "obiective_educative", "categorie", "sursa", "tags")
+
+    titlu = forms.CharField(required=True, label=u"Titlu", widget=TextInput(attrs={"style": "width: 100%; font-size: 24px; padding: 10px;"}))
+    min_durata_string = forms.CharField(required=False, label=u"Durată minimă", help_text=u"Folosește expresii de tipul 2h15m sau 1z3h30m sau 2h sau 12m")
+    max_durata_string = forms.CharField(required=False, label=u"Durată maximă", help_text=u"Folosește expresii de tipul 2h15m sau 1z3h30m sau 2h sau 12m")
+    descriere_joc = forms.CharField(required=True, label=u"Conținut", help_text=u"Format Markdown, descrierea jocului, ce trebuie pregătit, ce trebuie făcut, care este obiectivul, care sunt regulile ...", widget=MarkdownWidget())
+    descriere = forms.CharField(required=False, label=u"Descriere pe scurt", help_text=u"Un text de descriere pentru căutare", widget=Textarea(attrs={"style": "width: 100%; height: 80px"}))
+    ramuri_de_varsta = forms.ModelMultipleChoiceField(RamuraDeVarsta.objects.all(), required=True, label=u"Ramuri de vârstă", help_text=u"Alegeți mărcar una", widget=forms.CheckboxSelectMultiple)
+    tags = TagField(required=False, widget=TaggitTagsInput(attrs={"style": "width: 100%"}), label=u"Tag-uri")
+    materiale_necesare = forms.CharField(required=False, label=u"Materiale necesare", help_text=u"Câte unul pe linie, fără numerotare adițională", widget=Textarea(attrs={"style": "width: 100%; height: 80px"}))
+    obiective_educative = forms.CharField(required=False, label=u"Obiective educative", help_text=u"Câte unul pe linie, fără numerotare (se va face automat)", widget=Textarea(attrs={"style": "width: 100%; height: 80px"}))
+
+
+    def __init__(self, **kwargs):
+        super(FisaActivitateForm, self).__init__(**kwargs)
+        self.helper.form_class = "jocuri-form"
+        self.helper.layout = Layout(Field("titlu", css_class="input-xlarge"), Field("descriere"), Field("descriere_joc"),
+                                    Field("obiective_educative"), Field("materiale_necesare"),
+                                    Div(
+                                        Div(Field("min_durata_string"), Field("max_durata_string"), Field("sursa"), css_class="span4"),
+                                        Div(Field("categorie"), Field("min_participanti"), Field("max_participanti"),  css_class="span4"),
+                                        Div( Field("ramuri_de_varsta"), Field("tags"), css_class="span4"),
+                                        css_class="row-fluid"),
+                                    )
+
+    def clean(self):
+        if not "max_participanti" in self.cleaned_data and not "min_participanti" in self.cleaned_data:
+            raise ValidationError(u"Măcar un număr minim sau un număr maxim de participanți trebuie completat")
+        elif self.cleaned_data.get("min_participanti", 0) >= self.cleaned_data.get("max_participanti", 10000):
+            raise ValidationError(u"Minimul de participanți nu poate fi mai mare decât maximul")
+
+        if "max_durata_string" in self.cleaned_data and "min_durata_string" in self.cleaned_data:
+            if self.cleaned_data.get("min_durata_string") >= self.cleaned_data.get("max_durata_string"):
+                self.errors["min_durata_string"] = ["Durata minimă trebuie să fie mai mică decât cea maximă", ]
+                raise ValidationError(u"Durata minimă trebuie să fie mai mică decât cea maximă")
+
+        return self.cleaned_data
+
+    def clean_min_durata_string(self):
+        return self._clean_time_string(self.cleaned_data.get("min_durata_string", ""))
+
+    def clean_max_durata_string (self):
+        return self._clean_time_string(self.cleaned_data.get("max_durata_string", ""))
+
+    @staticmethod
+    def _clean_time_string(value):
+        return parse_string_to_seconds(value)
 
 class DocumentActivitateForm(CrispyBaseModelForm):
     class Meta:
