169a9082d05c70df826d18cb9ae8f791b3023034
Andrei Avram <andrei.avram@gmail.com>
2014-10-04 15:45:20 +0300

    adaugat suport pentru coloane aditionale in listade participanti adaugat suport pentru sumarizare (counts and sums)

--- scoutfile3/album/templatetags/customfield_tags.py
+++ scoutfile3/album/templatetags/customfield_tags.py
@@ -1,7 +1,10 @@
+from album.models import ParticipareEveniment
+
 __author__ = 'andrei'
 
 from django import template
 from django.template.defaultfilters import stringfilter
+import collections
 
 register = template.Library()
 
@@ -13,3 +16,28 @@ def valoare_camp(context, camp, participare):
 @register.simple_tag(takes_context=True)
 def camp_special(context, participare, camp):
     return participare.process_camp_aditional(camp)
+
+
+@register.inclusion_tag("album/breakdown.html", takes_context=True)
+def participare_breakdown(context, tip, target=None):
+    data = []
+    is_countable = True
+    eveniment = context.get("eveniment")
+    if tip in ("rol", "status_participare"):
+        data = [getattr(a, "get_%s_display" % tip)() for a in eveniment.participareeveniment_set.all()]
+
+    if tip in ("is_partial", ):
+        data = [a.is_partiala for a in eveniment.participareeveniment_set.all()]
+
+    if tip == "camp":
+        if target is None or not target.afiseaza_sumar:
+            return {}
+
+        data = [float(a.get_value()) for a in target.instante.all()]
+        if target.tip_camp == "number":
+            data = sum(data)
+            is_countable = False
+
+    if not is_countable:
+        return {"breakdown": {"Total": data}}
+    return {"breakdown": dict(collections.Counter(data))}
