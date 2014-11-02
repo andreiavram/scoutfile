28d555c783b9b43feb7d1e16cfb30d3bba551754
Andrei Avram <andrei.avram@gmail.com>
2014-11-02 18:35:07 +0200

    added migration added migration script

--- scoutfile3/adrese_postale/management/commands/update_adrese.py
+++ scoutfile3/adrese_postale/management/commands/update_adrese.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 import traceback
 from adrese_postale.adrese import AdresaPostalaException, AdresaPostala
 from adrese_postale.models import CodPostal
-from structuri.models import Membru
+from structuri.models import Membru, InformatieContact
 
 __author__ = 'andrei'
 
@@ -17,4 +17,24 @@ class Command(BaseCommand):
         #   pentru fiecare membru care are adresa
         #   prelucreaza adresa, vezi daca-i ok
         #   daca nu are cod, cauta-i codul, adauga si salveaza (?)
-        pass
+        infos = InformatieContact.objects.filter(tip_informatie__nume__iexact=u"Adresa corespondență")
+
+        new_codes = 0
+
+        for info in infos:
+            try:
+                adresa = AdresaPostala.parse_address(info.value, fail_silently=False)
+            except Exception, e:
+                self.stdout.write(str(e) + "\n")
+                continue
+
+            if not adresa.are_cod():
+                adresa.determine_cod()
+
+                if adresa.are_cod():
+                    info.value = adresa.__unicode__()
+                    self.stdout.write("info to save: %s\n" % info.value)
+                    new_codes += 1
+                else:
+                    self.stdout.write("could not determine code\n")
+
