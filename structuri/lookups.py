6a18eaf45348a1f3195a1c4667bc7163e63063bb
Andrei Avram <andrei.avram@gmail.com>
2015-02-14 05:10:22 +0200

    updated bower installed apps updated lodash added support for csrf in headers and jquery cookie script updated album to remove usage of dajax/ice fixed problems with lookups

--- structuri/lookups.py
+++ structuri/lookups.py
@@ -1,14 +1,15 @@
 # coding: utf-8
-'''
+"""
 Created on Sep 23, 2012
 
 @author: yeti
-'''
+"""
 from ajax_select import LookupChannel
-from structuri.models import Membru, Structura
 from django.db.models.query_utils import Q
 from django.template.loader import render_to_string
-from settings import STATIC_URL
+from django.conf import settings
+
+from structuri.models import Membru
 
 
 class MembriLookup(LookupChannel):
@@ -16,22 +17,24 @@ class MembriLookup(LookupChannel):
     search_field = "nume"
     
     def get_query(self, q, request):
-        qs = Membru.objects.filter(Q(nume__icontains=q) | Q(prenume__icontains = q))
-        qs = Membru.objects.filter(id__in=[m.id for m in qs if m.centru_local == request.user.utilizator.membru.centru_local])
+        qs = Membru.objects.filter(Q(nume__icontains=q) | Q(prenume__icontains=q))
+        membri_centru_local = [m.id for m in qs if m.centru_local == request.user.utilizator.membru.centru_local]
+        qs = Membru.objects.filter(id__in=membri_centru_local)
         return qs
 
     def check_auth(self, request):
         return True
     
     def format_match(self, obj):
-        return render_to_string("structuri/membru_for_ajax.html", {"obj" : obj, "STATIC_URL" : STATIC_URL})
+        return render_to_string("structuri/membru_for_ajax.html", {"obj": obj, "STATIC_URL": settings.STATIC_URL})
     
     def format_item_display(self, obj):
-        return render_to_string("structuri/membru_for_ajax.html", {"obj" : obj, "STATIC_URL" : STATIC_URL})
+        return render_to_string("structuri/membru_for_ajax.html", {"obj": obj, "STATIC_URL": settings.STATIC_URL})
+
 
 class LideriLookup(MembriLookup):
     def get_query(self, q, request):
-        qs = Membru.objects.filter(Q(nume__icontains=q) | Q(prenume__icontains = q))
+        qs = Membru.objects.filter(Q(nume__icontains=q) | Q(prenume__icontains=q))
         centru_local = request.user.utilizator.membru.centru_local
         qs = Membru.objects.filter(id__in=[m.id for m in qs if m.are_calitate("Lider", centru_local)])
         return qs
