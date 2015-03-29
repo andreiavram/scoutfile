d09267a51c98261802680172dbda17df7b178183
Andrei Avram <andrei.avram@gmail.com>
2015-03-30 01:15:58 +0300

    added some migrations

---
+++ cantece/migrations/0002_auto_20150314_1521.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import scoutfile3.s3utils
+import cantece.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cantece', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='templatecartecantece',
+            name='template_file',
+            field=models.FileField(storage=scoutfile3.s3utils.LocalStorage, upload_to=cantece.models.upload_to_template_carte_cantece),
+            preserve_default=True,
+        ),
+    ]
