47c404873faa3095aa1a9ec36de3c72e6c8c334c
Andrei Avram <andrei.avram@gmail.com>
2015-11-30 17:24:08 +0200

    updated fabfile

--- inventar/fabfile.py
+++ inventar/fabfile.py
@@ -1,6 +1,8 @@
 from fabric.api import run, env, cd
+from django.conf import settings
 
 
 def open_gate():
     with cd("/home/pi"):
+        env.password = settings.GATEKEEPER_CONNECTION_PASSWORD
         run("sudo python releu.py")
