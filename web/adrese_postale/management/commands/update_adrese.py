b4c041d88b2c815c53615c16bcbd1113cef476ff
narboom <chira.mircea.mc@gmail.com>
2018-05-07 21:42:13 +0300

    Refactored management commands that manage postal codes and addresses.

--- web/adrese_postale/management/commands/update_adrese.py
+++ web/adrese_postale/management/commands/update_adrese.py
@@ -1,51 +1,52 @@
-#   coding: utf8
+# coding: utf8
 from django.core.management.base import BaseCommand
 
 from adrese_postale.adrese import AdresaPostala
 from structuri.models import InformatieContact
 
-__author__ = 'andrei'
-
-import codecs, locale
-
 
 class Command(BaseCommand):
+
+    help = (
+        "For each membru with an address, check it's postal "
+        "code and if it doesn't have one create and save it."
+    )
+
     def handle(self, *args, **kwargs):
-        self.stdout = codecs.getwriter(locale.getpreferredencoding())(self.stdout)
-        #   pentru fiecare membru care are adresa
-        #   prelucreaza adresa, vezi daca-i ok
-        #   daca nu are cod, cauta-i codul, adauga si salveaza (?)
-        infos = InformatieContact.objects.filter(tip_informatie__nume__iexact=u"Adresa corespondență")
-        self.stdout.write("%d infos\n" % infos.count())
+        contact_data = InformatieContact.objects.filter(
+            tip_informatie__nume__iexact=u'Adresa corespondență')
+
+        self.stdout.write(
+            'Found valid contact information '
+            'from %d users.\n' % contact_data.count()
+        )
 
         new_codes = 0
 
-        for info in infos:
+        for info in contact_data:
             try:
-                adresa = AdresaPostala.parse_address(info.valoare, fail_silently=False)
+                adresa = AdresaPostala.parse_address(
+                    info.valoare, fail_silently=False)
             except Exception, e:
-                # self.stdout.write(u"%s" % unicode(e).encode('utf-8', 'ignore') + u"\n")
+                self.stdout.write(
+                    u'%s\n' % unicode(e))
                 continue
 
             if not adresa.are_cod():
                 try:
                     adresa.determine_cod()
                 except ValueError, e:
-                    self.stdout.write(unicode(e) + u"\n")
+                    self.stdout.write(u'%s\n' % unicode(e))
                     continue
 
-                if adresa.are_cod():
-                    info.valoare = adresa.__unicode__()
-                    info.save()
-                    # self.stdout.write(u"info to save: %s\n" % unicode(info.valoare).encode("utf-8"))
-                    new_codes += 1
-                else:
-                    self.stdout.write("could not determine code\n")
-            else:
-                if adresa.__unicode__() != info.valoare:
-                    info.valoare = adresa.__unicode__()
-                    info.save()
+                if not adresa.are_cod():
+                    self.stdout.write('Could not determine code\n')
+                    continue
 
+                new_codes += 1
 
-        self.stdout.write("%d new codes\n" % new_codes)
+            if adresa.__unicode__() != info.valoare:
+                info.valoare = adresa.__unicode__()
+                info.save()
 
+        self.stdout.write('%d new codes generated\n' % new_codes)
