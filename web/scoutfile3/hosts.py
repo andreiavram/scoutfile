7aeeeec390bb1742b45450c1bd819d0380fbde76
Andrei Avram <andrei.avram@gmail.com>
2023-02-22 00:48:45 +0200

    added scoutfile to host URLs (default url was not enough)

--- web/scoutfile3/hosts.py
+++ web/scoutfile3/hosts.py
@@ -3,4 +3,5 @@ from django_hosts import patterns, host
 host_patterns = patterns('',
     host(r'centru', 'scoutfile3.wagtail_urls', name='centru'),
     host(r'knowledge', 'scoutfile3.wagtail_urls', name='knowledge'),
+    host(r'scoutfile', 'scoutfile3.urls', name='scoutfile'),
 )
