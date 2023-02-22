9524fd8f005a8aacd8adf793d3d7ed5563be73e7
Andrei Avram <andrei.avram@gmail.com>
2023-02-22 02:12:56 +0200

    minor updates to pages fixed bs2 problem

--- web/pages/models.py
+++ web/pages/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from wagtail.admin.panels import FieldPanel
-from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel, StreamFieldPanel
+from wagtail.fields import RichTextField, StreamField
+from wagtail.images.blocks import ImageChooserBlock
 from wagtail.models import Page
-
+from wagtail import blocks
 
 # Create your models here.
 
@@ -13,3 +14,14 @@ class HomePage(Page):
         FieldPanel('body')
     ]
 
+
+class FreeFormPage(Page):
+    body = StreamField([
+        ('heading', blocks.CharBlock(form_classname="title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ], use_json_field=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body')
+    ]
