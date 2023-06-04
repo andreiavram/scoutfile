30fe0ff13cfc62c761ea0ee8f0bb9ecd5df7d661
Andrei Avram <andrei.avram@gmail.com>
2023-06-04 18:45:21 +0300

    POC for slack bot

--- web/slackbot/api/permissions.py
+++ web/slackbot/api/permissions.py
@@ -6,6 +6,6 @@ from slack_sdk.signature import SignatureVerifier
 class SlackVerifiedRequestPermission(BasePermission):
     def has_permission(self, request, view):
         verifier = SignatureVerifier(signing_secret=settings.SLACK_APP_SECRET)
-        timestamp = request.headers.get("x-slack-request-timestamp", ["0"])[0]
-        signature = request.headers.get("x-slack-signature", [""])[0]
+        timestamp = request.headers.get("x-slack-request-timestamp", ["0"])
+        signature = request.headers.get("x-slack-signature", [""])
         return verifier.is_valid(request.body, timestamp, signature)
--- web/slackbot/api/serializers.py
+++ web/slackbot/api/serializers.py
@@ -1,8 +1,10 @@
+from django.db.models import Q
 from rest_framework import serializers
 from django.conf import settings
 from slack_bolt import App
 
 from slackbot.slack_constants import SlackEventTypes
+from structuri.models import Membru
 
 app = App(
     token=settings.SLACK_BOT_TOKEN,
@@ -25,12 +27,41 @@ class SlackEventSerializer(serializers.Serializer):
     type = serializers.ChoiceField(choices=SlackEventTypes.choices)
     challenge = serializers.CharField(required=False)
     team_id = serializers.CharField(required=False)
-    event = SlackInnerEventSerializer()
+    event = SlackInnerEventSerializer(required=False)
 
     class Meta:
         fields = ["token", "type", "challenge", "team_id", "event"]
 
     def create(self, validated_data):
+        if "event" not in validated_data:
+            return validated_data
+
         # TODO: add queue mechanism here to deal with pubsub messages
-        app.client.chat_postMessage(channel=validated_data['event']['channel'], text=validated_data['event']['text'])
+
+        if validated_data["event"]["user"] == "U05AT2TT3PX":
+            # this is the current slack bot's uid
+            return validated_data
+
+        if validated_data["event"]["channel_type"] != "im":
+            return validated_data
+
+        user = app.client.users_profile_get(user=validated_data["event"]["user"])
+
+        email = user["profile"]["email"]
+        try:
+            membru = Membru.objects.get(Q(email__iexact=email) | Q(slack_email__iexact=email))
+        except Membru.DoesNotExist:
+            text = f"Emailul tau {email} nu este configurat în contul tău de Scoutfile. Vorbește cu yeti să rezolvi!"
+            app.client.chat_postMessage(channel=validated_data['event']['channel'], text=text)
+            return validated_data
+
+        inquiry = validated_data['event']['text'].lower()
+        if "cotizatie" in inquiry or "cotizație" in inquiry or "cotizația" in inquiry or "cotizatia" in inquiry:
+            text = f"Cotizația ta este {membru.status_cotizatie()}"
+        elif "ccl" in inquiry or "când e ccl" in inquiry or "cand e ccl" in inquiry or "cand e campul" in inquiry or "când e campul" in inquiry:
+            text = "Campul Centrului Local este între 12 și 20 august 2023"
+        else:
+            text = "Nu știu despre ce vorbești ..."
+
+        app.client.chat_postMessage(channel=validated_data['event']['channel'], text=text)
         return validated_data
