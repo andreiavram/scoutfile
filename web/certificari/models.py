c743d8e4dbe1743d8c9a02e9b5b011693e3f6d81
Andrei Avram <andrei.avram@gmail.com>
2023-07-21 13:25:56 +0300

    added support for expiring certificates #21

--- web/certificari/models.py
+++ web/certificari/models.py
@@ -1,5 +1,7 @@
+import datetime
+
 from django.db import models
-from django.db.models import TextChoices
+from django.db.models import TextChoices, Q
 
 
 class CertificationType(models.Model):
@@ -13,13 +15,21 @@ class CertificationType(models.Model):
     icon = models.ImageField(upload_to="certificari/icons/", null=True, blank=True)
     source = models.CharField(max_length=100, choices=SourceOptions.choices, default=SourceOptions.INTERNAL)
 
+    validity = models.DurationField(null=True, blank=True)
+
     def __str__(self):
         return self.title
+
+
+class ValidCertificateManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(Q(valid_until__isnull=True) | Q(valid_until__gte=datetime.date.today()))
 
 
 class Certificate(models.Model):
     issued_to = models.ForeignKey("structuri.Membru", on_delete=models.CASCADE, related_name="certificari")
     issued_on = models.DateField()
+    valid_until = models.DateField(null=True, blank=True)
 
     issued_by = models.CharField(max_length=1024, blank=True)
     certificate_type = models.ForeignKey(CertificationType, on_delete=models.CASCADE)
@@ -27,3 +37,17 @@ class Certificate(models.Model):
     event_received = models.ForeignKey("album.Eveniment", null=True, blank=True, on_delete=models.SET_NULL, related_name="issued_certificates")
     events = models.ManyToManyField("album.Eveniment", blank=True, related_name="contributed_certificates")
     document = models.ForeignKey("documente.Document", null=True, blank=True, on_delete=models.SET_NULL)
+
+    objects = ValidCertificateManager()
+    all_objects = models.Manager()
+
+    def save(self, *args, **kwargs):
+        if self.id is None and self.certificate_type.validity and self.valid_until is None:
+            self.valid_until = self.issued_on + self.certificate_type.validity
+        return super().save(*args, **kwargs)
+
+    @property
+    def is_valid(self):
+        return self.valid_until is None or self.valid_until > datetime.date.today()
+
+
